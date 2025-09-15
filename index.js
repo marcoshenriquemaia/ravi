554241027ebca5cf50d4fafe04660dcf630b9388
Marcos Maia <henriquemarcosmaia@gmail.com>
2025-09-15 14:14:50 -0300

    count up

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const countdown = (date) => {
-  const $countdown = document.querySelector('.countdown')
+const countup = (startDate) => {
   const now = new Date();
-  const distance = date - now;
-  const milliseconds = distance
-  const seconds = milliseconds / 1000
-  const minutes = seconds / 60
-  const hours = minutes / 60
-  const days = hours / 24
 
-  const fSeconds = (Math.floor(seconds) % 60).toString().padStart(2, '0')
-  const fMinutes = (Math.floor(minutes) % 60).toString().padStart(2, '0')
-  const fHours = (Math.floor(hours) % 24).toString().padStart(2, '0')
-  const fDays = (Math.floor(days)).toString().padStart(2, '0')
-
-  if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
-    $countdown.textContent = '00:00:00:00'
-
+  if (now < startDate) {
+    requestAnimationFrame(() => countup(startDate));
     return;
   }
 
-  $countdown.textContent = `${fDays}:${fHours}:${fMinutes}:${fSeconds}`
+  let years = now.getFullYear() - startDate.getFullYear();
+  let months = now.getMonth() - startDate.getMonth();
+  let days = now.getDate() - startDate.getDate();
+  let hours = now.getHours() - startDate.getHours();
+  let minutes = now.getMinutes() - startDate.getMinutes();
+  let seconds = now.getSeconds() - startDate.getSeconds();
 
-  setTimeout(() => countdown(date), 1000)
-}
+  if (seconds < 0) {
+    seconds += 60;
+    minutes--;
+  }
+  if (minutes < 0) {
+    minutes += 60;
+    hours--;
+  }
+  if (hours < 0) {
+    hours += 24;
+    days--;
+  }
+  if (days < 0) {
+    const prevMonth = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+    days += prevMonth;
+    months--;
+  }
+  if (months < 0) {
+    months += 12;
+    years--;
+  }
 
-countdown(new Date('10/29/2024 12:00:00'))
+  // Atualiza os spans
+  document.querySelector(".countdown-year").textContent = `${years
+    .toString()
+    .padStart(2, "0")} anos`;
+  document.querySelector(".countdown-month").textContent = `${months
+    .toString()
+    .padStart(2, "0")} meses`;
+  document.querySelector(".countdown-day").textContent = `${days
+    .toString()
+    .padStart(2, "0")} dias`;
+  document.querySelector(".countdown-hour").textContent = `${hours
+    .toString()
+    .padStart(2, "0")} horas`;
+  document.querySelector(".countdown-minute").textContent = `${minutes
+    .toString()
+    .padStart(2, "0")} minutos`;
+  document.querySelector(".countdown-second").textContent = `${seconds
+    .toString()
+    .padStart(2, "0")} segundos`;
+
+  setTimeout(() => countup(startDate), 1000);
+};
+
+// Exemplo
+countup(new Date("2024-10-19T22:10:00"));
